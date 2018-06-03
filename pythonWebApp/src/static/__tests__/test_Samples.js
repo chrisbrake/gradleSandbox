9be67c7d108ff6de16975e3b9bd1797e85a2b8b6
chrisbrake <nAp3ObtBG5yj>
2018-06-03 12:37:25 -0600

    added a parameterised test example.

---
+++ pythonWebApp/src/static/__tests__/test_Samples.js
@@ -0,0 +1,12 @@
+import each from 'jest-each';
+
+describe('Adding', () => {
+    each`
+    a    | b     | c
+    ${1} | ${1}  | ${2}
+    ${1} | ${2}  | ${3}
+    ${1} | ${10} | ${11}
+    `.test('$a + $b = $c', ({a, b, c}) => {
+        expect(a + b).toBe(c);
+    })
+});
