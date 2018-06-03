1e63feccdd734022eb80fe01fe0c97a28c29bc45
chrisbrake <nAp3ObtBG5yj>
2018-06-03 17:42:06 -0600

    Added an example skipped test

--- pythonWebApp/src/static/__tests__/test_Samples.js
+++ pythonWebApp/src/static/__tests__/test_Samples.js
@@ -1,6 +1,6 @@
 import each from 'jest-each';
 
-describe('Adding', () => {
+describe('Example test forms', () => {
     each`
     a    | b     | c
     ${1} | ${1}  | ${2}
@@ -8,5 +8,9 @@ describe('Adding', () => {
     ${1} | ${10} | ${11}
     `.test('$a + $b = $c', ({a, b, c}) => {
         expect(a + b).toBe(c);
+    })
+
+    test.skip('Test my important feature', () => {
+        // TODO
     })
 });
